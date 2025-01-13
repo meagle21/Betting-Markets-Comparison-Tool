be14d4310d3a6c770cca1e9d620a119696bcbd01
meagle21 <emanuel.eagle@gmail.com>
2025-01-13 15:32:36 -0500

    udpate

--- infra/sportsbooks_apis/main.tf
+++ infra/sportsbooks_apis/main.tf
@@ -2,7 +2,7 @@ module draftkings_basketball_team_api {
     source = "github.com/meagle21/Terraform-Module-Lambda-DynamoDB-Odds-Comparison"
     lambda_function_name = "draftkings_scraping_lambda_nba_team"
     sportsbook_website_url = "https://sportsbook.draftkings.com/leagues/basketball/nba"
-    dynambodb_table_name = "draftkings-basketball-team-odds"
+    dynambodb_table_name = ""
     sportsbook = "DraftKings"
     sport = "Basketball"
     team_odds_bool = false
